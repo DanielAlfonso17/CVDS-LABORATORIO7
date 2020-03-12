b0fddbfb1f27be87a9c0dad45dafc0c831a1073c
DanielAlfonso17 <daniel.alfonso-b@mail.escuelaing.edu.co>
2020-03-11 23:17:53 -0500

    Terminando Laboratorio

--- src/main/java/edu/eci/cvds/samples/services/client/MyBatisExample.java
+++ src/main/java/edu/eci/cvds/samples/services/client/MyBatisExample.java
@@ -1,32 +1,22 @@
-/*
- * Copyright (C) 2015 hcadavid
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package edu.eci.cvds.samples.services.client;
 
-
-
+import edu.eci.cvds.sampleprj.dao.mybatis.mappers.ClienteMapper;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import edu.eci.cvds.sampleprj.dao.mybatis.mappers.ClienteMapper;
+import edu.eci.cvds.sampleprj.dao.mybatis.mappers.ItemMapper;
+import edu.eci.cvds.samples.entities.Item;
+import edu.eci.cvds.samples.entities.TipoItem;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import static javax.xml.bind.DatatypeConverter.parseDate;
 
 /**
  *
@@ -63,21 +53,42 @@ public class MyBatisExample {
         SqlSessionFactory sessionfact = getSqlSessionFactory();
         SqlSession sqlss = sessionfact.openSession();
         ClienteMapper cm=sqlss.getMapper(ClienteMapper.class);
+        System.out.println("");
+        System.out.println("Imprimiendo consulta de clientes");
+        System.out.println("");
         System.out.println(cm.consultarClientes());
-        
-        //Crear el mapper y usarlo: 
-        //ClienteMapper cm=sqlss.getMapper(ClienteMapper.class)
-        //cm...
-        
-        
-        
+        System.out.println("");
+        System.out.println("Imprimiendo consulta de cliente Id");
+        System.out.println("");
+        System.out.println(cm.consultarCliente(6));
+
+        //cm.agregarItemRentadoACliente(6,15,parseDate("2020-03-03"),parseDate("2020-03-11"));
+        ItemMapper im = sqlss.getMapper(ItemMapper.class);
+        TipoItem tItem = new TipoItem(3,"Series");
+        Item item = new Item(tItem,1234,"NuevoItem","Item creado por fonso",parseDate("2020-03-03"),1543,"format","genero");
+        //im.insertarItem(item);
+
+        System.out.println("");
+        System.out.println("Imprimiendo consulta de items");
+        System.out.println("");
+        System.out.println(im.consultarItems());
+
+        System.out.println("");
+        System.out.println("Imprimiendo consulta de items id");
+        System.out.println("");
+        System.out.println(im.consultarItem(1234));
+
         sqlss.commit();
         
-        
         sqlss.close();
+    }
 
-        
-        
+    public static Date parseDate(String date) {
+        try {
+            return new SimpleDateFormat("yyyy-MM-dd").parse(date);
+        } catch (ParseException e) {
+            return null;
+        }
     }
 
 
